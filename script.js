271a8f744a2c91c53a8a430966e0ac702d72a7ab
adminib <Dill3rart@yandex.ru>
2025-05-27 09:45:32 +0500

    header ref

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var btn = document.getElementById('b-header-menu-btn');
+var btn = document.getElementById('b-header-menu');
 
 btn.addEventListener('click', ()=>{
-    var element = document.getElementsByClassName('b-header-menu-game');
+    var element = document.getElementsByClassName('b-header-menu');
     if (element[0].style !== 'display:none') 
         element[0].style = 'display:block';
-    else
-        element[0].style = 'display:block';
+    else if (element[0].style !== 'display:block') 
+         element[0].style = 'display:none';
 });
