e748ad3e3cd41aad5ff259ea1cd13fb89b7984d1
adminib <Dill3rart@yandex.ru>
2025-05-30 08:54:36 +0500

    final html+css - ref

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-$(".b-header-menu").on("click", ()=>{
-    var _hidden = $(".b-header-menu-ul");
-    if (_hidden.css("display") =="none")
-        _hidden.css("display","block");
-    else 
-        _hidden.css("display","none");
+const btn1 = document.getElementById('mab-square1');
+const walk = document.getElementById('main-walk');
+const list_btn = document.querySelectorAll('main-arena-btn'); 
+const menu = document.getElementById('b-header-menu-m');
+
+menu.addEventListener('click', ()=>{
+    let el = document.getElementById('b-header-menu-ul');
+    el.style.display = el.style.display ==='none' ? 'block' : 'none'
 });
+
+list_btn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        _setO(btn);
+        _walk('O');
+    })
+})
+
+function _walk(walk_text){
+    walk.innerText= 'Walk '+walk_text;
+}
+
+function _setX(btn){
+    btn.innerText = 'X'
+}
+
+function _setO(btn){
+    btn.innerText = 'O'
+}
