be2071540a4f066c4b06d183223180138280d2d1
adminib <Dill3rart@yandex.ru>
2025-06-02 16:31:58 +0500

    js check winner for replay game

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const walk = document.getElementById('main-walk')
-const menu = document.getElementById('b-header-menu-m')
-const el = document.getElementById('b-header-menu-ul')
 const scoreX = document.getElementById('score-X')
 const scoreO = document.getElementById('score-0')
-let _scoreX = 0
-let _scoreO = 0
+const data_score = {
+    _scoreX : 0,
+    _scoreO : 0,
+} 
 
 const btn1 = document.getElementById('mab-square1')
 const btn2 = document.getElementById('mab-square2')
@@ -15,8 +15,11 @@ const btn6 = document.getElementById('mab-square6')
 const btn7 = document.getElementById('mab-square7')
 const btn8 = document.getElementById('mab-square8')
 const btn9 = document.getElementById('mab-square9')
+const start = document.getElementById('footer')
+const replay = document.getElementById('start-replay')
 
-menu.addEventListener('click', menuOpen)
+
+start.addEventListener('click', play)
 btn1.addEventListener('click', clBtn)
 btn2.addEventListener('click', clBtn)
 btn3.addEventListener('click', clBtn)
@@ -30,9 +33,7 @@ btn9.addEventListener('click', clBtn)
 
 function clBtn(){
     let _move = walk.innerText.replace('Walk ','')
-    scoreX.innerText=_scoreX
-    scoreO.innerText=_scoreO
-    console.log(_move)
+    replay.innerText='Replay'
     if(this.innerText ===''){
         switch(_move){
         case 'X':
@@ -45,31 +46,55 @@ function clBtn(){
             break      
         }
     }
-    upScore(checkWinner())
+    if(checkWinner('X')){
+        upScore('X')
+        play()
+    }
+    else if (checkWinner('0')){
+        upScore('0')
+        play()
+    }
 }
 
 function upScore(winner){
     switch(winner){
         case 'X':
-            _scoreX++
+            data_score._scoreX++
             break
         case '0':
-            _scoreO++ 
+            data_score._scoreO++ 
             break
     }
 }
 
-function checkWinner(){
-    if (btn1.innerText === btn2.innerText === btn3.innerText)
-        return btn1.innerText
-    else if(btn1.innerText === btn4.innerText=== btn7.innerText)
-        return btn1.innerText
-}
-
-function menuOpen(){
-    el.style.display = el.style.display ==='none' ? 'block' : 'none'
+function checkWinner(check){
+    if (btn1.innerText === check && btn2.innerText === check && btn3.innerText === check ||
+        btn4.innerText === check && btn5.innerText === check && btn6.innerText === check ||
+        btn7.innerText === check && btn8.innerText === check && btn9.innerText === check ||
+        btn1.innerText === check && btn4.innerText === check && btn7.innerText === check ||
+        btn2.innerText === check && btn5.innerText === check && btn8.innerText === check ||
+        btn3.innerText === check && btn6.innerText === check && btn9.innerText === check ||
+        btn1.innerText === check && btn5.innerText === check && btn9.innerText === check ||
+        btn3.innerText === check && btn5.innerText === check && btn7.innerText=== check)
+        return true
 }
 
 function _walk(walk_text){
     walk.innerText= 'Walk '+walk_text;
+}
+
+function play(){
+    btn1.innerText=''
+    btn2.innerText=''
+    btn3.innerText=''
+    btn4.innerText=''
+    btn5.innerText=''
+    btn6.innerText=''
+    btn7.innerText=''
+    btn8.innerText=''
+    btn9.innerText=''
+    _walk('X')
+    scoreX.innerText=data_score._scoreX
+    scoreO.innerText=data_score._scoreO
+    replay.innerText='Replay'
 }
